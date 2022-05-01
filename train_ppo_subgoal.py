0f347debf56e5536301646936d8a8afa17fd5c55
Michel Schimpf <michelschimpf@MacBook-Pro-von-Michel.local>
2022-05-01 09:19:51 +0200

    .

--- train_ppo_subgoal.py
+++ train_ppo_subgoal.py
@@ -30,8 +30,8 @@ def train():
                              ])
     env_vec = VecMonitor(env_vec, "logs/PPO_0")
     model = ALGO('MlpPolicy', env_vec, verbose=1, tensorboard_log=logdir, n_steps=TIMESTEPS)
-    model = ALGO.load("models/PPO/3293184", env=env_vec)
-    iters = 135
+    # model = ALGO.load("models/PPO/3293184", env=env_vec)
+    iters = 0
     while True:
         print(iters)
         iters += 1
