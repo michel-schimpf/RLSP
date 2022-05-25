dd0bfcb832d11cd712d3d5023db4f82f280378a5
Michel Schimpf <michelschimpf@MacBook-Pro-von-Michel.local>
2022-05-25 16:00:47 +0200

    .

--- train_mt10_ppo_subgoal.py
+++ train_mt10_ppo_subgoal.py
@@ -15,9 +15,9 @@ def train():
     # variables:
     models_dir = f"models/PPO"
     logdir = "logs"
-    timestamps = 2048
-    number_envs_of_each_task = 6
-    batch_size = 4096
+    timestamps = 50#2048
+    number_envs_of_each_task = 1#6
+    batch_size = 50#4096
     rew_type = "rew1"
 
     # create env
