1284fff4728022c30fd59c5dc3e0bfd97dd4f3ba
Michel Schimpf <michelschimpf@MacBook-Pro-von-Michel.local>
2022-05-28 10:34:47 +0200

    .

--- train_mt10_ppo_subgoal.py
+++ train_mt10_ppo_subgoal.py
@@ -16,8 +16,9 @@ def train():
     models_dir = f"models/PPO"
     logdir = "logs"
     timestamps = 8192
-    number_envs_of_each_task = 6
+    number_envs = 61
     number_envs_per_task = [3,10,15,3,3,3,3,15,3,3]
+    # number_envs_per_task = [1]*10
     batch_size = 16384
     rew_type = "rew1"
 
@@ -27,13 +28,13 @@ def train():
 
     for i, (name, _) in enumerate(mt10.train_classes.items()):
         for _ in range(number_envs_per_task[i]):
-            env_array += make_env(name, rew_type, 10, i)
+            env_array.append(make_env(name, rew_type, 10, i))
 
     # for i in range(number_envs_of_each_task):
     #     env_array += [make_env(name, rew_type, 10, i) for i, (name, _) in enumerate(mt10.train_classes.items())]
 
     env_vec = SubprocVecEnv(env_array)
-    env_vec = RLPPAMonitor(env_vec, "logs/PPO", multi_env=True)
+    env_vec = RLPPAMonitor(env_vec, "logs/PPO_0", multi_env=True)
 
     # create or load model
     model = PPO('MlpPolicy', env_vec, verbose=1, tensorboard_log=logdir, n_steps=timestamps,
@@ -47,14 +48,14 @@ def train():
         i += 1
         model = model.learn(total_timesteps=timestamps, reset_num_timesteps=False,
                             tb_log_name="PPO", )
-        model.save(f"{models_dir}/{timestamps * i * number_envs_of_each_task * 10}")
+        model.save(f"{models_dir}/{timestamps * i * number_envs}")
 
 
 def make_env(name,rew_type,number_of_one_hot_tasks,one_hot_task_index):
 
     def _init():
         return SubGoalEnv(env=name, rew_type=rew_type, number_of_one_hot_tasks=number_of_one_hot_tasks,
-                      one_hot_task_index=one_hot_task_index)
+                          one_hot_task_index=one_hot_task_index)
     return _init
 
 
